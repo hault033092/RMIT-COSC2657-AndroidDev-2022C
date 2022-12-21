5cadab5d54bc52b41bee7767740d616a217afca1
Trung Hau Le <letrunghau090298@gmail.com>
2022-12-21 22:24:06 +0700

    initialize project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.myapplication.ui.buyback.BuybackFragment;
+import com.example.myapplication.ui.cart.CartFragment;
+import com.example.myapplication.ui.home.HomeFragment;
+import com.example.myapplication.ui.pcbuilder.PcBuilderFragment;
+import com.example.myapplication.ui.search.SearchFragment;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    CartFragment cartFragment = new CartFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    BuybackFragment buybackFragment = new BuybackFragment();
+    PcBuilderFragment pcBuilderFragment = new PcBuilderFragment();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.navigation_cart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,cartFragment).commit();
+                        return true;
+                    case R.id.navigation_search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,searchFragment).commit();
+                        return true;
+                    case R.id.navigation_pcbuilder:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,pcBuilderFragment).commit();
+                        return true;
+                    case R.id.navigation_buyback:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,buybackFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/pcbuilder/PcBuilderFragment.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui.pcbuilder;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class PcBuilderFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pcbuilder, container, false);
+    }
+}
