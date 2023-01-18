bf33974d7605f86bdb673248f97b41ae8f1272bb
KultrasHub <68211943+KultrasHub@users.noreply.github.com>
2023-01-18 18:32:14 +0700

    detail update

---
+++ app/src/main/java/com/example/myapplication/Entity/Voucher.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.Entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Voucher implements Serializable {
+    String voucherName;
+    String voucherDescription;
+    int fixedDiscount;
+    Date expiryDate;
+
+    public Voucher(String voucherName, String voucherDescription, int fixedDiscount, Date expiryDate) {
+        this.voucherName = voucherName;
+        this.voucherDescription = voucherDescription;
+        this.fixedDiscount = fixedDiscount;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getVoucherName() {
+        return voucherName;
+    }
+
+    public String getVoucherDescription() {
+        return voucherDescription;
+    }
+
+    public int getFixedDiscount() {
+        return fixedDiscount;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+}
