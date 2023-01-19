b5b5decabcb2677ee18694813af714e83be62353
KultrasHub <68211943+KultrasHub@users.noreply.github.com>
2023-01-19 16:27:01 +0700

    creating singleton

--- app/src/main/java/com/example/myapplication/Adapter/LocalItemsSingleton.java
+++ app/src/main/java/com/example/myapplication/Adapter/LocalItemsSingleton.java
@@ -1,7 +1,13 @@
 package com.example.myapplication.Adapter;
 
+import com.example.myapplication.Entity.Item;
+
+import java.util.ArrayList;
+
 public class LocalItemsSingleton {
     private  static LocalItemsSingleton instance = null;
 
+    ArrayList<Item> cart = new ArrayList<>();
 
+    
 }
