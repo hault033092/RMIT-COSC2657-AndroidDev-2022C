cb8578f774792e74311202c2bf1b15c3ad0c4478
LongJoe2002 <nguyentrongminhlong@gmail.com>
2023-01-20 17:12:31 +0700

    Update TestActivity.java

--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.myapplication.database.DatabaseManager;
+import com.example.myapplication.database.PcComponent;
+
+import java.util.ArrayList;
 
 public class TestActivity extends AppCompatActivity {
+    DatabaseManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        dbManager = new DatabaseManager(this);
+
+        ArrayList<PcComponent> components = dbManager.getAll();
+
+
+        TextView text = (TextView) findViewById(R.id.testText);
+        text.setText(components.get(0).getName());
+
     }
 }
